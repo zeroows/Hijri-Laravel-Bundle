a570b570411a0c5d4a6f87c5eca5e5dd9a7fa1c2
Abdulrhman Alkhodiry <almooheb@gmail.com>
2013-01-05 00:55:54 +0300

    Adding support for Localization also language folder with EN and AR

---
+++ language/en/hijri.php
@@ -0,0 +1,70 @@
+<?php
+
+return array(
+	
+	"month" => array(
+		"long" => array(
+		"1" => "Muharram",
+		"2" => "Safar",
+		"3" => "Rabi’ al-awwal",
+		"4" => "Rabi’ al-thani",
+		"5" => "Jumada al-awwal",
+		"6" => "Jumada al-thani",
+		"7" => "Rajab",
+		"8" => "Sha’ban",
+		"9" => "Ramadan",
+		"10" => "Shawwal",
+		"11" => "Dhu al-Qi’dah",
+		"12" => "Dhu al-Hijjah",
+		),
+
+		"short" => array(
+		"1" => "Muh",
+		"2" => "Saf",
+		"3" => "Rb1",
+		"4" => "Rb2",
+		"5" => "Jm1",
+		"6" => "Jm2",
+		"7" => "Raj",
+		"8" => "Shb",
+		"9" => "Rmd",
+		"10" => "Shw",
+		"11" => "DhQ",
+		"12" => "DhH",
+		),
+	),
+
+	"days" => array(
+		"long" => array(
+		"1" => "Monday",
+		"2" => "Tuesday",
+		"3" => "Wednesday",
+		"4" => "Thursday",
+		"5" => "Friday",
+		"6" => "Saturday",
+		"7" => "Sunday",
+		),
+
+		"short" => array(
+		"1" => "Mon",
+		"2" => "Tue",
+		"3" => "Wed",
+		"4" => "Thur",
+		"5" => "Fri",
+		"6" => "Sat",
+		"7" => "Sun",
+		),
+	),
+
+	"time" => array(
+		"long" => array(
+		"1" => "AM",
+		"2" => "PM",
+		),
+
+		"short" => array(
+		"1" => "am",
+		"2" => "pm",
+		),
+	),
+);
